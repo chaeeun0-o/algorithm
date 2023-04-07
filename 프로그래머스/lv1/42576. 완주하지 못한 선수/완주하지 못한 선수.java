24e9e701429268c8a153ea9c610a82e273fb8af8
inggaeng <122453216+inggaeng@users.noreply.github.com>
2023-04-07 23:25:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.60 ms, Memory: 95.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String,Integer>map = new HashMap<>();
+        for(String name : participant){
+            if(map.containsKey(name)){
+                map.put(name,map.get(name)+1);
+            }else{
+                map.put(name,1);
+            }
+        }
+        for(String name : completion){
+            if(map.get(name)>1){
+                map.put(name,map.get(name)-1);
+            }else{
+                map.remove(name);
+            }
+        }
+        for(String name : map.keySet()){
+            answer=name;
+        }
+        return answer;
+    }
+}
