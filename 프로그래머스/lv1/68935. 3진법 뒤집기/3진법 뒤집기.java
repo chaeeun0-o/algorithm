4e75a23d61cf8ded93cc91d84eb9b9c74fdf8f12
inggaeng <122453216+inggaeng@users.noreply.github.com>
2023-04-08 19:43:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,14 +1,6 @@
 class Solution {
-public int solution(int n) {
-int answer = 0;
-String s= Integer.toString(n,3);
-
-StringBuilder sb = new StringBuilder(s);
-String reverse = sb.reverse().toString();
-answer = Integer.parseInt(reverse,3);
-
-
-
-return answer;
-}
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
 }
