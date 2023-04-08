4c0135b8bbce1a721445e11242eb4f2db6c17664
inggaeng <122453216+inggaeng@users.noreply.github.com>
2023-04-08 14:26:14 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long a = num;
+        
+        while(a!=1){
+            if(a%2==0){
+            a/=2;
+            } else{
+            a=(3*a)+1;
+            } 
+            answer++;
+           
+        }
+         if(answer > 500){
+                answer=-1;
+            }
+        return answer;
+    }
+}
