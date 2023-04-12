c5901851b1468750b1317f15d0f42685a79dc4ef
inggaeng <122453216+inggaeng@users.noreply.github.com>
2023-04-12 22:54:23 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] reverseArr = new int[num_list.length];
+        
+        
+         for (int i = num_list.length - 1, j = 0; i >= 0; i--, j++) {
+            reverseArr[j] = num_list[i];
+        }
+        
+        return reverseArr;
+    }
+}
