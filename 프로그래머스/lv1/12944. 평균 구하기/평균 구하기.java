9c40e2a2fe90c7cabec69f99c54f6358b8bccdb3
inggaeng <122453216+inggaeng@users.noreply.github.com>
2023-04-12 15:43:57 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int sum = 0;
+        
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        answer = (sum*100/100.0)/arr.length;
+        return answer;
+    }
+}
