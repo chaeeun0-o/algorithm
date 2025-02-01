a0a99905d2a4a5fde9fa368e4c7a07d331371149
chaeeun0-o <122453216+chaeeun0-o@users.noreply.github.com>
2025-02-01 17:21:12 +0900

    [Bronze II] Title: 단어의 개수, Time: 404 ms, Memory: 53808 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine().trim();
+        if (input.isEmpty()) {
+            System.out.println(0);
+        } else {
+            String[] words = input.split("\\s+");
+            System.out.println(words.length);
+        }
+
+        br.close();
+    }
+}
