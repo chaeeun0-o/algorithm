489d9ddafc331b8a252ee96e77f1927508585d33
chaeeun0-o <122453216+chaeeun0-o@users.noreply.github.com>
2025-03-17 20:49:29 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 120 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int[] alphabet = new int[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            alphabet[c - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            System.out.print(alphabet[i] + " ");
+        }
+
+    }
+}
